43a1eb72b4e9e063aa000f8ae0c4eb19eed4a2c5
Hugh Rawlinson <hughr2005@gmail.com>
2024-08-18 18:38:03 +0200

    Set up prettier

--- src/app/apps/page.tsx
+++ src/app/apps/page.tsx
@@ -1,32 +1,43 @@
 import Link from "next/link";
 import styles from "./apps.module.css";
-import {getApps, getCategories, getDevices} from "@/badgehub-api-client/generated/swagger/public/public";
-import {Filter} from "@/components/Filter";
+import {
+  getApps,
+  getCategories,
+  getDevices,
+} from "@/badgehub-api-client/generated/swagger/public/public";
+import { Filter } from "@/components/Filter";
 
 export interface SearchParams {
-    category: string;
-    device: string;
+  category: string;
+  device: string;
 }
 
-export default async function Listing({searchParams} : { searchParams: Partial<SearchParams> }) {
-    const [apps, categories, devices]
-        = await Promise.all([getApps(searchParams), getCategories(), getDevices()])
+export default async function Listing({
+  searchParams,
+}: {
+  searchParams: Partial<SearchParams>;
+}) {
+  const [apps, categories, devices] = await Promise.all([
+    getApps(searchParams),
+    getCategories(),
+    getDevices(),
+  ]);
 
-    return (
-        <>
-            <Filter categories={categories.data} devices={devices.data}/>
+  return (
+    <>
+      <Filter categories={categories.data} devices={devices.data} />
 
-            {apps.data.map((app) => (
-                <article className={styles.appCard} key={app.slug}>
-                    <Link href={`/apps/${app.slug}`}>
-                        <h2>{app.name}</h2>
-                    </Link>
-                    <Link href={`/categories/${app.category_slug}`}>
-                        {app.category_slug}
-                    </Link>
-                    <p>{app.user_name}</p>
-                </article>
-            ))}
-        </>
-    );
+      {apps.data.map((app) => (
+        <article className={styles.appCard} key={app.slug}>
+          <Link href={`/apps/${app.slug}`}>
+            <h2>{app.name}</h2>
+          </Link>
+          <Link href={`/categories/${app.category_slug}`}>
+            {app.category_slug}
+          </Link>
+          <p>{app.user_name}</p>
+        </article>
+      ))}
+    </>
+  );
 }
--- src/components/Filter/index.tsx
+++ src/components/Filter/index.tsx
@@ -1,53 +1,80 @@
 "use client";
 
 import styles from "./Filter.module.css";
-import {useRef} from "react";
-import {useSearchParams, useRouter} from "next/navigation";
-import {Category, Device} from "@/badgehub-api-client/generated/models";
+import { useRef } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+import { Category, Device } from "@/badgehub-api-client/generated/models";
 
-export function Filter({categories, devices}: {categories: Category[], devices: Device[]}) {
-    const router = useRouter();
-    const params = useSearchParams();
+export function Filter({
+  categories,
+  devices,
+}: {
+  categories: Category[];
+  devices: Device[];
+}) {
+  const router = useRouter();
+  const params = useSearchParams();
 
-    const inputCategoryRef = useRef<HTMLSelectElement>(null);
-    const inputDeviceRef = useRef<HTMLSelectElement>(null);
+  const inputCategoryRef = useRef<HTMLSelectElement>(null);
+  const inputDeviceRef = useRef<HTMLSelectElement>(null);
 
-    const setUrlParams = () => {
-        const searchParams = new URLSearchParams();
+  const setUrlParams = () => {
+    const searchParams = new URLSearchParams();
 
-        if (inputCategoryRef.current) {
-            if (inputCategoryRef.current.value != "") {
-                searchParams.set('category', inputCategoryRef.current.value);
-            } else {
-                searchParams.delete('category')
-            }
-        }
-
-        if (inputDeviceRef.current) {
-            if (inputDeviceRef.current.value != "") {
-                searchParams.set('device', inputDeviceRef.current.value);
-            } else {
-                searchParams.delete('device')
-            }
-        }
-
-        router.replace(`?${searchParams}`);
+    if (inputCategoryRef.current) {
+      if (inputCategoryRef.current.value != "") {
+        searchParams.set("category", inputCategoryRef.current.value);
+      } else {
+        searchParams.delete("category");
+      }
     }
 
-    return <section className={styles.appFilter}>
-        Filter:
-        <select name="categories" id="categories" onChange={setUrlParams} ref={inputCategoryRef} defaultValue={params.get('category') || ""}>
-        <option value="" key="all">All</option>
-        {categories.map((category) => (
-            <option value={category.slug} key={category.slug}>{category.name}</option>
-        ))}
-        </select>
+    if (inputDeviceRef.current) {
+      if (inputDeviceRef.current.value != "") {
+        searchParams.set("device", inputDeviceRef.current.value);
+      } else {
+        searchParams.delete("device");
+      }
+    }
 
-        <select name="devices" id="devices" onChange={setUrlParams} ref={inputDeviceRef} defaultValue={params.get('device') || ""}>
-        <option value="" key="all">All</option>
-        {devices.map((category) => (
-            <option value={category.slug} key={category.slug}>{category.name}</option>
+    router.replace(`?${searchParams}`);
+  };
+
+  return (
+    <section className={styles.appFilter}>
+      Filter:
+      <select
+        name="categories"
+        id="categories"
+        onChange={setUrlParams}
+        ref={inputCategoryRef}
+        defaultValue={params.get("category") || ""}
+      >
+        <option value="" key="all">
+          All
+        </option>
+        {categories.map((category) => (
+          <option value={category.slug} key={category.slug}>
+            {category.name}
+          </option>
         ))}
-        </select>
+      </select>
+      <select
+        name="devices"
+        id="devices"
+        onChange={setUrlParams}
+        ref={inputDeviceRef}
+        defaultValue={params.get("device") || ""}
+      >
+        <option value="" key="all">
+          All
+        </option>
+        {devices.map((category) => (
+          <option value={category.slug} key={category.slug}>
+            {category.name}
+          </option>
+        ))}
+      </select>
     </section>
+  );
 }
