cf7965875320cb604094c56f077650e0d185f1d1
Hugh Rawlinson <hughr2005@gmail.com>
2024-08-18 18:25:20 +0200

    Add docker compose

--- src/app/apps/[slug]/page.tsx
+++ src/app/apps/[slug]/page.tsx
@@ -1,11 +1,12 @@
-import { getApp } from "@/badgehub-api-client";
+import { getAppDetails } from "@/badgehub-api-client/generated/swagger/public/public";
 
 export default async function AppPage({
   params,
 }: {
   params: { slug: string };
 }) {
-  const app = await getApp(params.slug);
+  const response = await getAppDetails(params.slug);
+  const app = response.data;
 
   return (
     <article>
@@ -16,7 +17,7 @@ export default async function AppPage({
       <h3>Supported Devices:</h3>
       <ul>
         {app.devices.map((device) => (
-          <li key={device.slug}>{JSON.stringify(device, null, 2)}</li>
+          <li key={device}>{JSON.stringify(device, null, 2)}</li>
         ))}
       </ul>
     </article>
