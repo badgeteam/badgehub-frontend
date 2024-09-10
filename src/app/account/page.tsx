10ebd81ba05abc526a4332fa2aae8b653432d631
Edwin Martin <edwin@bitstorm.org>
2024-09-10 17:22:14 +0200

    Use loading indicator

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import {SessionProvider, useSession} from "next-auth/react"
+import {SessionProvider} from "next-auth/react"
 import {LoginButton} from "@/components/LoginButton";
 import {Account} from "@/components/Account";
 
--- src/components/LoginButton/index.tsx
+++ src/components/LoginButton/index.tsx
@@ -2,19 +2,28 @@ import {useSession, signIn, signOut} from "next-auth/react"
 import styles from "./LoginButton.module.css";
 
 export function LoginButton() {
-    const {data: session} = useSession();
+    const {data: session, status} = useSession();
 
-    if (session) {
-        return (
-            <div className={styles.container}>
+    let html = <></>;
+
+    switch (status) {
+        case "loading":
+            html = <p>...</p>;
+            break;
+        case "unauthenticated":
+            html = <button className={styles.button} onClick={() => signIn()}>Sign in</button>;
+            break;
+        case "authenticated":
+            html = <>
                 {session?.user?.email}
                 <button className={styles.button} onClick={() => signOut()}>Sign out</button>
-            </div>
-        )
+            </>
+            break;
     }
+
     return (
         <div className={styles.container}>
-            <button className={styles.button} onClick={() => signIn()}>Sign in</button>
+            {html}
         </div>
     )
 }
