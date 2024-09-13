30f89e688c1c1086b520292cbafc634ff58d22b3
Edwin Martin <edwin@bitstorm.org>
2024-09-13 17:57:31 +0200

    Indicate current tab

--- src/components/MainNav/index.tsx
+++ src/components/MainNav/index.tsx
@@ -1,13 +1,22 @@
+"use client"
 import Link from "next/link";
 import styles from "./MainNav.module.css";
+import {usePathname} from "next/navigation";
+import clsx from "clsx";
 
 export function MainNav() {
+    const pathname = usePathname();
+
+    console.log('pathname', pathname);
+
     return (
         <nav className={styles.mainNav}>
-            <Link href={"/apps"}>
+            <Link href={"/apps"} className={pathname == "/apps" ? styles.currentLink : ""}
+                  aria-current={pathname == "/apps" ? "page" : "false"}>
                 <h1>Apps</h1>
             </Link>
-            <Link href={"/account"}>
+            <Link href={"/account"} className={pathname == "/account" ? styles.currentLink : ""}
+                  aria-current={pathname == "/account" ? "page" : "false"}>
                 <h1>Account</h1>
             </Link>
         </nav>
