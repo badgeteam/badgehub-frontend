36b63f6167cb2bc2a7ef9d7f32b0a595695e300e
Edwin Martin <edwin@bitstorm.org>
2024-09-13 17:07:50 +0200

    Moce app list to own component

--- src/app/apps/page.tsx
+++ src/app/apps/page.tsx
@@ -1,12 +1,4 @@
-import Link from "next/link";
-import styles from "./apps.module.css";
-import {
-  getApps,
-  getCategories,
-  getDevices,
-} from "@/badgehub-api-client/generated/swagger/public/public";
-import { Filter } from "@/components/Filter";
-import { SessionProvider } from "next-auth/react"
+import {AppList} from "@/components/AppList";
 
 export interface SearchParams {
   category: string;
@@ -18,27 +10,12 @@ export default async function Listing({
 }: {
   searchParams: Partial<SearchParams>;
 }) {
-  const [apps, categories, devices] = await Promise.all([
-    getApps(searchParams),
-    getCategories(),
-    getDevices(),
-  ]);
+
+
 
   return (
     <>
-      <Filter categories={categories.data} devices={devices.data} />
-
-      {apps.data.map((app) => (
-        <article className={styles.appCard} key={app.slug}>
-          <Link href={`/apps/${app.slug}`}>
-            <h2>{app.name}</h2>
-          </Link>
-          <Link href={`/categories/${app.category_slug}`}>
-            {app.category_slug}
-          </Link>
-          <p>{app.user_name}</p>
-        </article>
-      ))}
+      <AppList searchParams={searchParams}/>
     </>
   );
 }
---
+++ src/components/AppList/index.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import styles from "./AppList.module.css";
+import {
+    getApps,
+    getCategories,
+    getDevices,
+} from "@/badgehub-api-client/generated/swagger/public/public";
+import { Filter } from "@/components/Filter";
+
+export interface SearchParams {
+    category: string;
+    device: string;
+}
+
+export async function AppList({
+                                          searchParams,
+                                      }: {
+    searchParams: Partial<SearchParams>;
+}) {
+
+
+    const [apps, categories, devices] = await Promise.all([
+        getApps(searchParams),
+        getCategories(),
+        getDevices(),
+    ]);
+
+    return (
+        <>
+            <Filter categories={categories.data} devices={devices.data} />
+
+            {apps.data.map((app) => (
+                <article className={styles.appCard} key={app.slug}>
+                    <Link href={`/apps/${app.slug}`}>
+                        <h2>{app.name}</h2>
+                    </Link>
+                    <Link href={`/categories/${app.category_slug}`}>
+                        {app.category_slug}
+                    </Link>
+                    <p>{app.user_name}</p>
+                </article>
+            ))}
+        </>
+    );
+}
