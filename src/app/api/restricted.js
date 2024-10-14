a3a185e3a6cb031aea269cf4908d8f5aa4f70b4a
Edwin Martin <edwin@bitstorm.org>
2024-10-14 12:15:33 +0200

    Fix ESLint warning to not export arrow functions

--- src/app/api/restricted.js
+++ src/app/api/restricted.js
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "./auth/[...nextauth]";
 
-export default async (req, res) => {
+export default async function(req, res) {
   const session = await getServerSession(req, res, authOptions);
 
   if (session) {
