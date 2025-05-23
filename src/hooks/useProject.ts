aa2dbbbdc94a428de86bd19df29c26690dbb05f0
Francis Duvivier <francisduvivier@gmail.com>
2025-05-23 22:23:55 +0200

    NO-TICKET: fix using latest version

--- src/hooks/useProject.ts
+++ src/hooks/useProject.ts
@@ -3,15 +3,19 @@ import {
   getDraftProject,
   getDraftProjectResponse200,
 } from "@/badgehub-api-client/generated/swagger/private/private";
+import {
+  getProject,
+  getProjectResponse,
+} from "@/badgehub-api-client/generated/swagger/public/public";
 
 export const useProject = (projectSlug: string) => {
   const [projectDetails, setProjectDetails] = useState<
-    getDraftProjectResponse200["data"] | undefined
+    getProjectResponse["data"] | undefined
   >(undefined);
   const [projectCacheBuster, setProjectCacheBuster] = useState({});
   const triggerUpdate = () => setProjectCacheBuster({});
   useEffect(() => {
-    getDraftProject(projectSlug as string).then((r) => {
+    getProject(projectSlug as string).then((r) => {
       if (r.status !== 200) {
         throw new Error("Failed to fetch project details");
       }
