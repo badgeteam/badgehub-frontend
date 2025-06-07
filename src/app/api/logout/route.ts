172d57dfbb2633b4f67f049a88564abaa5bbe855
Edwin Martin <edwin@bitstorm.org>
2025-06-07 12:57:01 +0200

    feat(auth): Create placeholder Auth POC page

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -2,7 +2,8 @@ import { redirect } from "next/navigation";
 
 function handler() {
   redirect(
-    `${process.env.KEYCLOAK_ISSUER}/protocol/openid-connect/logout?client_id=${encodeURIComponent(process.env.KEYCLOAK_ID!)}`,
+    // `${process.env.KEYCLOAK_ISSUER}/protocol/openid-connect/logout?client_id=${encodeURIComponent(process.env.KEYCLOAK_ID!)}&post_logout_redirect_uri=${encodeURIComponent(process.env.BADGEHUB_API_BASEURL!)}`,
+    `${process.env.KEYCLOAK_ISSUER}/protocol/openid-connect/logout?client_id=${encodeURIComponent(process.env.KEYCLOAK_ID!)}&post_logout_redirect_uri=${encodeURIComponent("https://badgehub.p1m.nl/")}`,
   );
 }
 
